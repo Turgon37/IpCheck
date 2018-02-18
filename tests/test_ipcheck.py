f06db807c8865a6b92c21644360a031b976e1400
Pierre GINDRAUD <pgindraud@gmail.com>
2018-02-19 00:53:15 +0100

    add cmdline test for version

--- tests/test_ipcheck.py
+++ tests/test_ipcheck.py
@@ -16,6 +16,10 @@ def test_cmdline():
     """Must produce an error is no url was given"""
     result = subprocess.Popen(shlex.split('./ipcheck.py --help'), stdout=subprocess.PIPE)
     stdout, stderr = result.communicate()
+    assert 'usage:' in stdout.decode()
+
+    result = subprocess.Popen(shlex.split('./ipcheck.py --version'), stdout=subprocess.PIPE)
+    stdout, stderr = result.communicate()
     assert 'IpCheck version' in stdout.decode()
 
     result = subprocess.Popen(shlex.split('./ipcheck.py --url-v4 "localhost/"'), stdout=subprocess.PIPE)
